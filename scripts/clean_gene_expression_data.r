8ddfd3157da36cce8b3083c0d1911b0573296ab9
Richard Qian <richardqiancs@gmail.com>
2023-10-10 12:59:37 -0400

    Remove negative values from data by shifting it

--- scripts/clean_gene_expression_data.r
+++ scripts/clean_gene_expression_data.r
@@ -1,9 +1,10 @@
-# This script will remove the 'EnsEMBL' and 'all_hugo_ids' columns from the gene expression data, and remove rows with "NA" HUGO IDs.
+# This script will remove the 'Ensembl' and 'all_hugo_ids' columns from the gene expression data, and remove rows with "NA" HUGO IDs.
 # This script will also remove any samples that were not collected from the Anterior Cingulate Cortex (AnCg) from Control and Schizophrenia patients.
 # For our project, we are verifying the results of the original study to compare differences between Control and Schizophrenia patients in the AnCg.
 
 # install.packages("data.table")
 library(data.table)
+library(magrittr)
 
 # Load in the gene expression data that has already been mapped to HUGO
 gene_expression_data <- fread("./data/SRP073813/SRP073813-HUGO.tsv", header = TRUE, sep = "\t")
@@ -30,5 +31,20 @@ selected_samples <- metadata %>%
 final_gene_expression_data <- gene_expression_data_cleaned %>%
     dplyr::select(first_mapped_hugo, all_of(selected_samples$refinebio_accession_code))
 
+# Now, we will shift data so negative values are 0
+
+# Find the min of each row. If it's negative, add the abs(min) to each value in the row so that the min is 0
+adjust_values <- function(row) {
+    numerical_cols <- which(sapply(final_gene_expression_data, is.numeric))
+    row_vals <- as.numeric(row[numerical_cols])
+    min_value <- min(row_vals)
+    if(min_value < 0) {
+        row[numerical_cols] <- as.numeric(row[numerical_cols]) + abs(min_value)
+    }
+    return(row)
+}
+
+output_df <- as.data.frame(t(apply(final_gene_expression_data, 1, adjust_values)))
+
 # Write the cleaned/filtered data to a new file
-write.table(final_gene_expression_data, "SRP073813-HUGO-cleaned.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+write.table(output_df, "SRP073813-HUGO-cleaned.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
