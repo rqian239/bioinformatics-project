11fbe2dcd9c652477389064585cfc05b9b36630d
Richard Qian <richardqiancs@gmail.com>
2023-10-09 23:22:51 -0400

    Load in metadata to start filtering samples

--- scripts/clean_gene_expression_data.r
+++ scripts/clean_gene_expression_data.r
@@ -1,3 +1,7 @@
+# This script will remove the 'EnsEMBL' and 'all_hugo_ids' columns from the gene expression data, and remove rows with "NA" HUGO IDs.
+# This script will also remove any samples that were not collected from the Anterior Cingulate Cortex (AnCg) from Control and Schizophrenia patients.
+# For our project, we are verifying the results of the original study to compare differences between Control and Schizophrenia patients in the AnCg.
+
 # install.packages("data.table")
 library(data.table)
 
@@ -10,6 +14,30 @@ gene_expression_data[, (columns_to_remove) := NULL]
 
 # Remove rows with "NA" HUGO IDs
 gene_expression_data_cleaned <- gene_expression_data[!is.na(gene_expression_data$first_mapped_hugo)]
+
+# Now we will filter the data so that only samples collected from the Anterior Cingulate Cortex (ACC) from Control and Schizophrenia patients are included.
+
+# Read in metadata TSV file
+metadata_file <- "./data/SRP073813/metadata_SRP073813.tsv"
+metadata <- readr::read_tsv(metadata_file)
+
+# Label psychiatric disorder
+metadata <- metadata %>%
+  dplyr::mutate(psy_disorder = dplyr::case_when(
+    stringr::str_detect(refinebio_title, "_C_") ~ "Control",
+    stringr::str_detect(refinebio_title, "_M_") ~ "Major Depression",
+    stringr::str_detect(refinebio_title, "_B_") ~ "Bipolar Disorder",
+    stringr::str_detect(refinebio_title, "_S_") ~ "Schizophrenia"
+  ))
+
+# Label brain region
+metadata <- metadata %>%
+  dplyr::mutate(brain_region = dplyr::case_when(
+    stringr::str_detect(refinebio_title, "AnCg") ~ "Anterior Cingulate Cortex",
+    stringr::str_detect(refinebio_title, "nAcc") ~ "Nucleus Accumbens",
+    stringr::str_detect(refinebio_title, "DLPFC") ~ "Dorsolateral Prefrontal Cortex"
+  ))
+
 
 # Write the cleaned data to a new file
 write.table(gene_expression_data_cleaned, "SRP073813-HUGO-cleaned.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
