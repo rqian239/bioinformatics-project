9f1bc002465ab4c97f428cc7791ed003c8c6a6cf
Richard Qian <richardqiancs@gmail.com>
2023-10-31 13:57:06 -0400

    Edit alluvial plot to include all clusters

--- scripts/alluvial.r
+++ scripts/alluvial.r
@@ -4,16 +4,16 @@ library(ggalluvial)
 library(dplyr)
 
 # Load in cluster data
-k_means_clusters <- readr::read_tsv("./results/clustering/k-means-cluster-results.tsv")
+clustering_results <- readr::read_tsv("./results/clustering/all-cluster-results.tsv")
 
-# Combine data into one dataframe to plot
-clustering_results <- k_means_clusters # Combine clustering results here
-
+clustering_results$k_cluster <- paste("k-", clustering_results$k_cluster, sep = "")
+clustering_results$hierarchical_cluster <- paste("h-", clustering_results$hierarchical_cluster, sep = "")
+clustering_results$pam_cluster <- paste("p-", clustering_results$pam_cluster, sep = "")
 
 # Create alluvial plot
 ggplot(data = clustering_results,
-    aes(axis1 = disorder_group, axis2 = k_cluster)) +
-    scale_x_discrete(limits = c("disorder_group", "k_cluster"), expand = c(.2, .05)) + 
+    aes(axis1 = disorder_group, axis2 = k_cluster, axis3 = hierarchical_cluster, axis4 = pam_cluster)) +
+    scale_x_discrete(limits = c("disorder_group", "k_cluster", "hierarchical_cluster", "pam_cluster"), expand = c(.2, .05)) + 
     xlab("Grouping") +
     geom_alluvium(aes(fill = disorder_group)) +
     geom_stratum() +
